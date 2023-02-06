01af2ce91f6d3d686752c5b4bc286d135aec5d2d
Yazid <108950946+yazid-k@users.noreply.github.com>
2023-02-06 17:25:09 +0100

    header and makefiles

---
+++ philosophers/philo/philo.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekadiri <ekadiri@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/06 17:17:03 by ekadiri           #+#    #+#             */
+/*   Updated: 2023/02/06 17:22:28 by ekadiri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <string.h>
+#endif
