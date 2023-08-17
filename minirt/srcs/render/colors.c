7ec144b8faef57a545f9458fb42135e94e5c7ee9
Yazid <108950946+yazid-k@users.noreply.github.com>
2023-08-17 01:01:40 +0100

    Working on shadows

--- minirt/srcs/render/colors.c
+++ minirt/srcs/render/colors.c
@@ -6,30 +6,45 @@
 /*   By: ekadiri <ekadiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/29 17:42:08 by ekadiri           #+#    #+#             */
-/*   Updated: 2023/08/16 15:25:38 by ekadiri          ###   ########.fr       */
+/*   Updated: 2023/08/17 01:01:14 by ekadiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minirt.h"
 
-int	shadow(t_coord h, t_data *data)
+int	shadow_func(t_elem *light, t_elem *elem, t_elem *e, t_coord p)
 {
+	t_coord	n;
+	t_coord	origin;
 	t_ray	r;
+
+	n = normal(p, e);
+	origin = vec_add(p, vec_scale(n, 0.001));
+	r = ray(origin, norm(vec_sub(light->coord, origin)));
+	if (!isnan(hit(r, elem).x) && (e->type == SPHERE || e->type == PLANE))
+	{
+		n = vec_scale(n, -1);
+		origin = vec_add(p, vec_scale(n, 0.001));
+		r = ray(origin, norm(vec_sub(light->coord, origin)));
+	}
+	if (!isnan(hit(r, elem).x))
+		return (0);
+	return (1);
+}
+
+int	shadow(t_coord h, t_data *data, t_elem *e)
+{
 	t_elem	*light;
 	t_elem	*elem;
 
 	light = get_elem_by_type(data, LIGHT);
 	elem = data->elem;
-	r = ray(h, vec_sub(light->coord, h));
-	r.origin.x += 0.0001 * r.dir.x;
-	r.origin.y += 0.0001 * r.dir.y;
-	r.origin.z += 0.0001 * r.dir.z;
 	while (elem)
 	{
 		if (elem->type >= 3)
 		{
-			if (isnan(hit(r, elem).x) == 0)
-				return (1);
+			if (!shadow_func(light, elem, e, h))
+				return (0);
 		}
 		elem = elem->next;
 	}
@@ -59,15 +74,13 @@ int	light(t_coord p, t_data *data, t_elem *elem)
 	light = get_elem_by_type(data, LIGHT);
 	if (!light)
 		return (ambient_color(elem->rgb, data));
-	if (!shadow(p, data))
+	if (!shadow(p, data, elem))
 		return (0);
 	n = normal(p, elem);
 	ratio = fmax(0, vec_dot(n, norm(vec_sub(light->coord, p)))) * light->light;
 	if ((elem->type == PLANE || elem->type == CIRCLE) && !ratio)
 	{
-		n.x = -n.x;
-		n.y = -n.y;
-		n.z = -n.z;
+		n = vec_scale(n, -1);
 		ratio = fmax(0, vec_dot(n, norm(vec_sub(light->coord, p))));
 	}
 	return (rgb(get_r(elem->rgb) * ratio, get_g(elem->rgb) * ratio,
--- minirt/srcs/render/render.c
+++ minirt/srcs/render/render.c
@@ -6,7 +6,7 @@
 /*   By: ekadiri <ekadiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 11:29:46 by ekadiri           #+#    #+#             */
-/*   Updated: 2023/08/15 19:31:59 by ekadiri          ###   ########.fr       */
+/*   Updated: 2023/08/17 00:50:40 by ekadiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ void	cast_ray(t_data *data, int i, int j, double viewport[2])
 	t_coord	bl;
 	double	k[2];
 	t_coord	direction;
+	int		color;
 
 	bl = coord(-viewport[0] / 2., -viewport[1] / 2., -1.);
 	k[0] = (double)i / (700 * 16 / 9);
@@ -51,8 +52,9 @@ void	cast_ray(t_data *data, int i, int j, double viewport[2])
 	direction = matrix_mult(data->mat, direction);
 	direction = vec_sub(direction, data->cam->coord);
 	r = ray(data->cam->coord, norm(direction));
-	if (ray_color(r, data))
-		my_mlx_pixel_put(data, i, 700 - j, ray_color(r, data));
+	color = ray_color(r, data);
+	if (color)
+		my_mlx_pixel_put(data, i, 700 - j, color);
 }
 
 void	render(t_data *data)
